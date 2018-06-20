aa50aed23004e29390e5feecdd274db70699258d
vhparadajr <vhparadajr@gmail.com>
2018-06-19 23:53:06 -0400

    Final NASA

--- Nasa/js/main.js
+++ Nasa/js/main.js
@@ -1,25 +1,13 @@
 // user can input
 
 // user clicks button search api for photo of user date
-
-
-$.get("https://api.nasa.gov/planetary/apod?api_key=G1HURmLK4MdyhAPtU0Cze9d9asWjfNuTy2WRpYsW", function(res){
-console.log(res);
+$(document).ready(function(){
+  $("button").on("click", function(){
+    var date = $("#date").val();
+    var source = "https://api.nasa.gov/planetary/apod?api_key=G1HURmLK4MdyhAPtU0Cze9d9asWjfNuTy2WRpYsW&date=" + date;
+    // alert(mez);
+    $.get(source, function(res){
+      $("img").attr("src",res.hdurl);
+    });
+  });
 });
-
-
-
-
-
-
-
-
-// $("button").on("click", function(){
-//   var mez = Number($("#month").val())
-//   var dia = Number($("#day").val())
-//   var ano = Number($("#year").val())
-//   alert(mez)
-// });
-//
-//
-$("img").attr("src","https://apod.nasa.gov/apod/image/1805/TerminatorMoon_Zia_960.jpg");
